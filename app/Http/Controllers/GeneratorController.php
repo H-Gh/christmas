6e8dbae4d79174fcdb72ebfa6403829963a18f6c
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2020-07-18 16:33:21 +0400

    Add the main controller to generate and render the shapes

---
+++ app/Http/Controllers/GeneratorController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Library\ShapeGenerator\AlignCenterRowGenerator;
+use App\Library\RowsRenderers\HtmlRowsRenderer;
+use App\Library\Shapes\ShapeFactory;
+use Fox\Controller\Controller;
+use Fox\Helpers\Request;
+use Throwable;
+
+/**
+ * The Controller to handle posts actions
+ * PHP version >= 7.0
+ *
+ * @category Controllers
+ * @package  Paper
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     null
+ */
+class GeneratorController extends Controller
+{
+    /**
+     * @return void
+     */
+    public function index()
+    {
+        $this->render("/christmas/index");
+    }
+
+    /**
+     * @return void
+     * @throws Throwable
+     */
+    public function show()
+    {
+        $shape = ShapeFactory::create(Request::get("type"));
+        $shape->setHeight(Request::get("height"));
+        $rowGenerator = new AlignCenterRowGenerator($shape);
+        $rows = $rowGenerator->getRows();
+        $shapeRenderer = new HtmlRowsRenderer();
+        $this->render("/christmas/render", [
+            "shape" => $shapeRenderer->render($rows)
+        ]);
+    }
+}
